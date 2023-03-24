351e2737e73422a9e85e37d70c721f1570cab485
joyshaheb <khondokoralam@gmail.com>
2023-03-25 01:09:51 +0600

    features added

--- src/components/ProfileCard/ProfileCard.tsx
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -17,7 +17,7 @@ const ProfileCard: React.FC<CardProps> = ({
     <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <img className="rounded-t-lg w-full" src={picture} alt="" />
       <div className="p-5">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
           {title} {firstName} {lastName}
         </h5>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
@@ -35,6 +35,13 @@ const ProfileCard: React.FC<CardProps> = ({
             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-700 dark:hover:bg-red-800 dark:focus:ring-red-800"
           >
             <i className="bi bi-trash3-fill"></i>
+          </button>
+
+          <button
+            onClick={() => console.log(id)}
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-600 rounded-lg hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-700 dark:hover:bg-green-800 dark:focus:ring-green-800"
+          >
+            <i className="bi bi-pencil-fill"></i>
           </button>
         </div>
       </div>
--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useGetAllUsersQuery, useDeleteUserMutation } from "../store";
 import Loader from "../components/Loader/Loader";
 import ProfileCard from "../components/ProfileCard/ProfileCard";
 import { IPerson } from "../types";
 import DeleteLoader from "../components/Loader/DeleteLoader/DeleteLoader";
+import Pagination from "../components/Pagination/Pagination";
 
 const Users = () => {
+  const [stats, setStats] = useState({
+    limit: 0,
+    page: 0,
+    total: 75,
+    totalPages: 10 / 10,
+  });
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = 5;
+
+  console.log(currentPage);
+
   const { data, isLoading, error } = useGetAllUsersQuery({});
   console.log("data", data);
   const [deleteUser, { isLoading: deleteIsLoading, error: deleteError }] =
@@ -19,15 +31,37 @@ const Users = () => {
     } finally {
     }
   };
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  useEffect(() => {
+    // setStats((prevStats) => ({
+    //   ...prevStats,
+    //   total: stats.length,
+    //   totalPages: Math.ceil(stats.length / prevStats.limit),
+    // }));
+  }, []);
+
   return isLoading ? (
     <Loader />
   ) : (
-    <div className="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-      {deleteIsLoading && <DeleteLoader message="Deleting user, please wait" />}
-      {data?.data?.map((item: IPerson) => (
-        <ProfileCard key={item?.id} {...item} onDelete={onDelete} />
-      ))}
-    </div>
+    <>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+      />
+      <div className="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+        {deleteIsLoading && (
+          <DeleteLoader message="Deleting user, please wait" />
+        )}
+        {data?.data?.map((item: IPerson) => (
+          <ProfileCard key={item?.id} {...item} onDelete={onDelete} />
+        ))}
+      </div>
+    </>
   );
 };
 
